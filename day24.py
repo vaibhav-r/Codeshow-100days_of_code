809b89d9e36b2a600aa1683e6b87680c10cd02f9
vaibhav-r <77624981+vaibhav-r@users.noreply.github.com>
2021-02-10 09:42:36 +0530

    Create day24.py

---
+++ day24.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    if(year % 4 == 0) :
+        if(year % 100 ==0) and (year % 400 ==0):
+            leap=True
+    else :
+        leap=False
+    # Write your logic  here
+    
+    return leap
